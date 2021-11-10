33932179fdb7b9103a244d99235ee7dc708be24e
Sidney Andrews <v-sandrews@microsoft.com>
2021-11-09 19:30:47 -0500

    Wave 2 commit

---
+++ 08-sdk-bulk/script.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.Azure.Cosmos;
+
+string endpoint = "<cosmos-endpoint>";
+string key = "<cosmos-key>";
