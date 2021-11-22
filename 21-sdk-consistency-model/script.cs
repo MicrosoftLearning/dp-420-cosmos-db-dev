e89fa89c39db206640b9c861a46fe9d49250f786
Sidney Andrews <v-sandrews@microsoft.com>
2021-11-21 23:00:05 -0500

    Wave 4 Commit

--- 21-sdk-consistency-model/script.cs
+++ 21-sdk-consistency-model/script.cs
@@ -6,12 +6,3 @@ string key = "<cosmos-key>";
 using CosmosClient client = new CosmosClient(endpoint, key);
 
 Container container = client.GetContainer("cosmicworks", "products");
-
-var item = new Product("", "Example Product", "");
-
-ItemRequestOptions options = new()
-{ 
-    ConsistencyLevel = ConsistencyLevel.Eventual 
-};
-
-await container.CreateItemAsync<Product>(item, new PartitionKey(""), requestOptions: options);
