358a1a574a64d2ee8bbeff529f7f6c2432d76897
Sidney Andrews <v-sandrews@microsoft.com>
2021-11-21 21:52:30 -0500

    Wave 4 Commit

--- 20-sdk-regions/script.cs
+++ 20-sdk-regions/script.cs
@@ -2,16 +2,3 @@ using Microsoft.Azure.Cosmos;
 
 string endpoint = "<cosmos-endpoint>";
 string key = "<cosmos-key>";
-
-CosmosClientOptions options = new () 
-{ 
-    ApplicationPreferredRegions = new List<string>{ "westus", "eastus" }
-};
-
-using CosmosClient client = new(endpoint, key, options);
-
-Container container = client.GetContainer("cosmicworks", "products");
-
-int? currentThroughput = await container.ReadThroughputAsync();
-
-Console.WriteLine($"Current Throughput:\t{currentThroughput}");
