a561adc958b0e5cfbea38f9b8a2b1385560ee9f0
MScalopez <87540998+MScalopez@users.noreply.github.com>
2021-11-22 06:53:05 -0600

    Add files via upload

---
+++ 28-key-vault/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _28_key_vault.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
