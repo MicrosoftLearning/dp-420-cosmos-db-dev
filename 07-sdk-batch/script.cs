52970265372c5bd4355b6c2343773abd590c5fba
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-05-04 14:04:49 -0500

    Update script.cs

--- 07-sdk-batch/script.cs
+++ 07-sdk-batch/script.cs
@@ -4,13 +4,7 @@ using Microsoft.Azure.Cosmos;
 string endpoint = "<cosmos-endpoint>";
 string key = "<cosmos-key>";
 
-CosmosClientOptions clientoptions = new CosmosClientOptions()
-{
-    RequestTimeout = new TimeSpan(0,0,90)
-    , OpenTcpConnectionTimeout = new TimeSpan (0,0,90)
-};
-
-CosmosClient client = new CosmosClient(endpoint, key, clientoptions);
+CosmosClient client = new CosmosClient(endpoint, key);
     
 Database database = await client.CreateDatabaseIfNotExistsAsync("cosmicworks");
 Container container = await database.CreateContainerIfNotExistsAsync("products", "/categoryId", 400);
