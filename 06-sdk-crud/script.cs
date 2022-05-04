98a7825ac93cf737978b0cb1bcff28e3ea4bc567
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-05-04 13:57:53 -0500

    Update script.cs

--- 06-sdk-crud/script.cs
+++ 06-sdk-crud/script.cs
@@ -4,10 +4,4 @@ using Microsoft.Azure.Cosmos;
 string endpoint = "<cosmos-endpoint>";
 string key = "<cosmos-key>";
 
-CosmosClientOptions clientoptions = new CosmosClientOptions()
-{
-    RequestTimeout = new TimeSpan(0,0,90)
-    , OpenTcpConnectionTimeout = new TimeSpan (0,0,90)
-};
-
-CosmosClient client = new CosmosClient(endpoint, key, clientoptions);
+CosmosClient client = new CosmosClient(endpoint, key);
