a3ff2d9e61bdc1c46e436183be28e79e564029ac
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-05-04 14:13:51 -0500

    Update script.cs

--- 13-change-feed/script.cs
+++ 13-change-feed/script.cs
@@ -4,10 +4,4 @@ using static Microsoft.Azure.Cosmos.Container;
 string endpoint = "<cosmos-endpoint>";
 string key = "<cosmos-key>";
 
-CosmosClientOptions clientoptions = new CosmosClientOptions()
-{
-    RequestTimeout = new TimeSpan(0,0,90)
-    , OpenTcpConnectionTimeout = new TimeSpan (0,0,90)
-};
-
-CosmosClient client = new CosmosClient(endpoint, key, clientoptions);
+CosmosClient client = new CosmosClient(endpoint, key);
