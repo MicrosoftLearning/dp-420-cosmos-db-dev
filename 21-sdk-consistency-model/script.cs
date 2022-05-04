b287b1652faa1265bd32d637aac12f65ff3cf02c
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-05-04 14:17:15 -0500

    Update script.cs

--- 21-sdk-consistency-model/script.cs
+++ 21-sdk-consistency-model/script.cs
@@ -3,12 +3,6 @@ using Microsoft.Azure.Cosmos;
 string endpoint = "<cosmos-endpoint>";
 string key = "<cosmos-key>";
 
-CosmosClientOptions clientoptions = new CosmosClientOptions()
-{
-    RequestTimeout = new TimeSpan(0,0,90)
-    , OpenTcpConnectionTimeout = new TimeSpan (0,0,90)
-};
-
-CosmosClient client = new CosmosClient(endpoint, key, clientoptions);
+CosmosClient client = new CosmosClient(endpoint, key);
 
 Container container = client.GetContainer("cosmicworks", "products");
