3be4b800741e1a634b21c7c57421fab45e597bdd
Santaji Suryawanshi <s.suryawanshi@jfhillebrand.com>
2024-03-01 19:25:16 +0100

    cosmosdb feed processor exercise

--- 13-change-feed/script.cs
+++ 13-change-feed/script.cs
@@ -5,3 +5,35 @@ string endpoint = "<cosmos-endpoint>";
 string key = "<cosmos-key>";
 
 CosmosClient client = new CosmosClient(endpoint, key);
+
+Container sourceContainer = client.GetContainer("cosmicworks", "products");
+Container leaseContainer = client.GetContainer("cosmicworks", "productslease");
+
+ChangesHandler<Product> handleChanges = async (
+    IReadOnlyCollection<Product> changes,
+    CancellationToken cancellationToken
+) => {
+    Console.WriteLine($"START\tHandling batch of changes...");
+    foreach(Product product in changes)
+    {
+        await Console.Out.WriteLineAsync($"Detected Operation:\t[{product.id}]\t{product.name}");
+    }
+};
+
+var builder = sourceContainer.GetChangeFeedProcessorBuilder<Product>(
+        processorName: "productsProcessor",
+        onChangesDelegate: handleChanges
+    );
+
+ChangeFeedProcessor processor = builder
+    .WithInstanceName("consoleApp")
+    .WithLeaseContainer(leaseContainer)
+    .Build();
+
+await processor.StartAsync();
+
+Console.WriteLine($"RUN\tListening for changes...");
+Console.WriteLine("Press any key to stop");
+Console.ReadKey();
+
+await processor.StopAsync();
